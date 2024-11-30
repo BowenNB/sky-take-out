a81208006c50ae0722412b6f70453ad703f4680a
BowenNB <2923591580@qq.com>
2024-11-30 11:33:37 +0800

    菜品分页查询业务功能代码开发

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 @Service
 @Slf4j
-public class DishSeriviceImpl implements DishService {
+public class DishServiceImpl implements DishService {
 
     @Autowired
     private DishMapper dishMapper;
