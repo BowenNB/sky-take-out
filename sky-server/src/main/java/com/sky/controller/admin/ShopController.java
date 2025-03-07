299b75f87e63668a4d1cd387ad543d87d47b8a19
BowenNB <2923591580@qq.com>
2025-03-07 14:30:42 +0800

    修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
